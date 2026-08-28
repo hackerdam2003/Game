096bf57fa2862b1a808eff90f7a3b5dadc89c209
hackerdam2003 <hackerdam2003@gmail.com>
2026-08-28 13:48:13 +0530

    Implement matchmaking engine in server.js
    Added a matchmaking engine that runs every 2 seconds.

--- server.js
+++ server.js
@@ -1,1 +1,5 @@
-
+import { runMatchmaking } from './core/matchmaker.js';
+// 🧠 Start The Master Matchmaker Engine (Runs every 2 seconds)
+setInterval(() => {
+    runMatchmaking(connectedPlayers, io);
+}, 2000);
